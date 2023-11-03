18be9962232d652e347c7258a42fde44cd84b896
ldy9155 <ldy9155@naver.com>
2023-11-04 03:55:31 +0900

    feat: 자동차 경주 2단계-문자열 덧셈 계산기 및 1단계-SetCollection Test 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -6,14 +6,16 @@ import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
-    void split(){
-        String[] result = "1,2".split(",");
+    @DisplayName("Split 테스트")
+    public void split(){
+        java.lang.String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1","2");
     }
 
     @Test
-    void substring(){
-        String result = "(1,2)".substring(1, 4);
+    @DisplayName("SubString 테스트")
+    void subString(){
+        java.lang.String result = "(1,2)".substring(1,4);
         assertThat(result).isEqualTo("1,2");
     }
 
@@ -21,10 +23,8 @@ public class StringTest {
     @DisplayName("예외 발생 테스트")
     void charAt(){
         assertThatThrownBy(() -> {
-            char result = "abc".charAt(0);
-            throw new IndexOutOfBoundsException("인덱스");
-
+            char result = "abc".charAt(5);
         }).isInstanceOf(IndexOutOfBoundsException.class)
-                .hasMessageContaining("인덱스 범위를 초과했습니다.");
+                .hasMessageContaining("index out of range");
     }
 }
